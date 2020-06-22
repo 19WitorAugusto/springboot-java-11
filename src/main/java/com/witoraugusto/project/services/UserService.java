4f83be48f2be24a5b17d903fb75b3e3479c7e6df
witor augusto <witorsantosaugusto@gmail.com>
2020-06-22 15:51:38 -0300

    Service layer, component registration

---
+++ src/main/java/com/witoraugusto/project/services/UserService.java
@@ -0,0 +1,25 @@
+package com.witoraugusto.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.witoraugusto.project.entities.User;
+import com.witoraugusto.project.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();//buscar todos!
+	}
+	//retornando por ID
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
